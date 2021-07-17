727b05f45625ba8c96db7a57a4d675ae3695b525
Luiz Filipe S Mariz <lfsmariz@gmail.com>
2021-07-17 16:10:21 -0300

    rumo ao 100%

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 const net = require('net');
+const os = require('os');
+
+const arch = os.arch();
+const cpus = os.cpus();
+const totalmem = os.freemem();
 
 const { getLocationInfos } = require('./location');
 
@@ -10,19 +15,31 @@ const getHeaderValue = (data, header) => {
   return headerData.split(': ').pop();
 };
 
-const startOfResponse = null;
+const startOfResponse = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n';
 
-const endOfResponse = null;
+const endOfResponse = '\r\n\r\n';
 
 const server = net.createServer((socket) => {
+  console.log('Conectado');
   socket.on('data', (data) => {
-    const clientIP = null;
+    const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
+    const device = getHeaderValue(data.toString(), 'User-Agent');
 
     getLocationInfos(clientIP, (locationData) => {
       socket.write(startOfResponse);
       socket.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
+      socket.write(`<p data-testid="ip">${clientIP}</p>`);
+      socket.write(`<p data-testid="city">${locationData.city}</p>`);
+      socket.write(`<p data-testid="postal_code">${locationData.postal_code}</p>`);
+      socket.write(`<p data-testid="region">${locationData.region}</p>`);
+      socket.write(`<p data-testid="country">${locationData.country}</p>`);
+      socket.write(`<p data-testid="company">${locationData.company}</p>`);
+      socket.write(`<p data-testid="device">${device}</p>`);
+      socket.write(`<p data-testid="arch">Arch: ${arch}</p>`);
+      socket.write(`<p data-testid="cpu">CPU: ${cpus[0].model} x ${cpus.length}</p>`);
+      socket.write(`<p data-testid="memory">Total memory: ${totalmem / 1024 / 1024 / 1024}GB</p>`);
       socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
       socket.write('</body></html>');
       socket.write(endOfResponse);
